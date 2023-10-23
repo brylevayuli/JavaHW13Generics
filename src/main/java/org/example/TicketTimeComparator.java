2e4e648f5c6897c90203dfbc5d38a109a12c49ce
Julia <yinwhite88@gmail.com>
2023-10-23 23:01:15 +0300

    correct code

--- src/main/java/org/example/TicketTimeComparator.java
+++ src/main/java/org/example/TicketTimeComparator.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import org.example.Ticket;
 
 import java.util.Comparator;
--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -1,5 +1,6 @@
 import org.example.AviaSouls;
 import org.example.Ticket;
+import org.example.TicketTimeComparator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
